f620157f0307420c9d366e1816e4d97a78adee61
Trishul-creator <gtrish2010@gmail.com>
2024-11-16 18:36:54 -0600

    first commit

---
+++ src/main.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       gurumurthyvenkataraman                                       #
+# 	Created:      11/16/2024, 6:36:09 PM                                       #
+# 	Description:  IQ2 project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello IQ2")
